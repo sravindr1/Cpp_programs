2741912dd11f5e093601afe6c08be9a87bcaf480
sarala <sravindr@mtu.edu>
2018-06-23 11:08:15 -0400

    Hello world with namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-	std::cout<<"Hello, world!\n"; // print hello wolrld to uotput console
+	//std::cout<<"Hello, world!\n"; // print hello wolrld to output console
+	cout<<"Hello, World (using namespace) \n"; // print hello wolrld to output console
 }
