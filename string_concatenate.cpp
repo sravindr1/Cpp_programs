88eeea6d4d2de5e2290b0a1178d81e09be5db65b
sarala <sravindr@mtu.edu>
2018-06-25 20:44:18 -0400

    string concatenate

---
+++ string_concatenate.cpp
@@ -0,0 +1,12 @@
+//File name: string_concatenate.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string str1 = "Hi!";
+	string str2 = "How are you";
+	string str = str1+str2;
+	cout<<str;
+	return 0;
+	}
